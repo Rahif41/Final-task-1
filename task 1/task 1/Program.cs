8692e215b3019def2e33e6b77a9146a11da9789c
Rahif hasan <18-36365-1@student.aiub.edu>
2020-11-23 23:12:18 +0600

    Final task 1

---
+++ task 1/task 1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a2 = new SavingsAccount("alif", "1122", 10000);        
+            a2.transaction(9580);
+            Console.WriteLine();
+            a2.transaction(1000);
+            a2.transaction(1000);
+            a2.transaction(1000);
+            a2.transaction(1000);
+            a2.transaction(1000); 
+
+            a2.transaction(1000); 
+            a2.ShowInfo();
+            Console.WriteLine();
+
+
+            FixedAccount a4 = new FixedAccount("alam ", "2123", 2020, 15000); 
+            a4.transaction(2000);
+            a4.ShowInfo();
+            Console.WriteLine();
+
+            SpecialSavingsAccount a1 = new SpecialSavingsAccount("azad", "2233", 1000);      
+            SpecialSavingsAccount a7 = new SpecialSavingsAccount("azad", "2200", 500);
+
+            a1.transaction(100);
+            a1.transaction(100);
+            a1.transaction(100);
+            a1.transaction(100);
+            a1.transaction(100);
+            a1.transaction(100); 
+
+            a7.transaction(450); 
+            a1.ShowInfo();
+            a7.ShowInfo();
+            Console.WriteLine();
+
+
+            Account a3 = new OverDraftAccount("asad ", "245", 5000);     
+            a3.transaction(5750);
+            a3.ShowInfo();
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ task 1/task 1/savings account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class SavingsAccount : Account
+    {
+        int trans = 0;
+
+        public SavingsAccount(string name, string id, double balance) : base(name, id, balance)
+        {
+
+        }
+
+        override public void transaction(double ammount)
+        {
+
+            if (trans >= 5)
+            {
+                Console.WriteLine(" The maximum transaction time commited");
+            }
+            else if (this.balance - amount < 500)
+            {
+                Console.WriteLine(" The transaction couldn't be completed due to balance less than 500$");
+            }
+            else
+            {
+                this.balance = this.balance - amount;
+                trans++;
+            }
+
+        }
+        override public void ShowInfo()
+        {
+            Console.WriteLine("Name :" + name + " ID :" + id + "  Balance :" + balance);
+        }
+    }
+}
